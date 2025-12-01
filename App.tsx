99970e5645a99be066f09127a378543dde46eba6
Harun Saracevic <harunsaracevicc@gmail.com>
2025-12-01 03:21:50 +0100

    Analytics fixes and improvements

--- App.tsx
+++ App.tsx
@@ -7,6 +7,8 @@ import Gallery from './pages/Gallery';
 import Contact from './pages/Contact';
 import { LanguageProvider } from './contexts/LanguageContext';
 
+import { Analytics } from '@vercel/analytics/react';
+
 const App: React.FC = () => {
   return (
     <LanguageProvider>
@@ -20,6 +22,7 @@ const App: React.FC = () => {
           </Routes>
         </Layout>
       </HashRouter>
+      <Analytics />
     </LanguageProvider>
   );
 };
